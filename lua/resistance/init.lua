da722c0b4b4d77e2204226a5af635bb1373912ad
Amin Foroutan <aminforoutan7@gmail.com>
2025-11-22 15:54:56 +0330

    [ref]

--- lua/resistance/init.lua
+++ lua/resistance/init.lua
@@ -7,7 +7,7 @@ vim.api.nvim_set_hl(0, "SpellCap",  { undercurl = true, sp = "#00b3ff" })
 vim.api.nvim_set_hl(0, "SpellRare", { undercurl = true, sp = "#00b3ff" })
 vim.api.nvim_set_hl(0, "SpellLocal",{ undercurl = true, sp = "#00b3ff" })
 vim.opt.spelloptions:append("camel")
-
+vim.opt.clipboard = "unnamedplus"
 require("resistance.remap")
 require("resistance.lsp")
 require("resistance.appearance")
