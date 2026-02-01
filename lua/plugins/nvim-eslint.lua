1b589e1abc29297b44acf67084f7ae112f7f1b26
Amin Foroutan <aminforoutan7@gmail.com>
2026-02-01 17:23:16 +0330

    [ref]

--- lua/plugins/nvim-eslint.lua
+++ lua/plugins/nvim-eslint.lua
@@ -1,6 +1,11 @@
 return {
   'esmuellert/nvim-eslint',
   config = function()
-    require('nvim-eslint').setup({})
+    require('nvim-eslint').setup({
+      format = true,
+      codeActionOnSave = {
+        ["source.fixAll"] = "always",  -- Apply all fixable issues on save
+      }
+    })
   end,
 }
